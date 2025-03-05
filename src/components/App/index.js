db6e87eab7c11418ee5e33c531c2638614a22a37
Renato Santos <renato_jfs@hotmail.com>
2025-03-05 15:44:23 -0300

    Configurando o React Router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,27 @@
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import GlobalStyles from '../../assets/styles/global';
 
+import { Routes } from '../../Routes';
+import GlobalStyles from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 
 import { Header } from '../Header';
-import { ContactsList } from '../ContactsList';
+// import { ContactsList } from '../ContactsList';
 
 import { Container } from './styles';
 
 function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
-      <Container>
-        <Header />
-        <ContactsList />
-      </Container>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+        <Container>
+          <Header />
+          <Routes />
+          {/* <ContactsList /> */}
+        </Container>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
