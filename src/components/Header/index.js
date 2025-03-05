7ee6e73e0b041c0c1f9ad6773ab914bfe8d77f6f
Renato Santos <renato_jfs@hotmail.com>
2025-03-04 23:26:30 -0300

    Criando o Header da aplicação

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import { Container, InputSearchContainer } from './styles';
+import Logo from '../../assets/images/logo.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <img src={Logo} alt="MyContacts" width="201" />
+      <InputSearchContainer>
+        <input
+          type="text"
+          placeholder="Pesquisar contato..."
+        />
+      </InputSearchContainer>
+    </Container>
+  );
+}
