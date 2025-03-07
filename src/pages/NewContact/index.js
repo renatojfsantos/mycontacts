4ddfcaf1fb632b517b56735e91c92ee09afa7174
Renato Santos <renato_jfs@hotmail.com>
2025-03-06 22:20:46 -0300

    Criando os componentes Input e Select

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,9 +1,25 @@
+import { Input } from '../../components/Input';
 import { PageHeader } from '../../components/PageHeader';
+import { Select } from '../../components/Select';
 
 export function NewContact() {
   return (
-    <PageHeader
-      title="Novo Contato"
-    />
+    <>
+      <PageHeader
+        title="Novo Contato"
+      />
+
+      <Input type="text" placeholder="Nome" />
+
+      <Select>
+        <option value="0">Selecione o canal</option>
+        <option value="1">E-mail</option>
+        <option value="2">Telefone</option>
+        <option value="3">WhatsApp</option>
+        <option value="4">Facebook</option>
+        <option value="5">Instagram</option>
+        <option value="6">LinkedIn</option>
+      </Select>
+    </>
   );
 }
