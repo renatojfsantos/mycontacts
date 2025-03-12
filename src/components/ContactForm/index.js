3279a9b9646ab3bb6aa3945fd3d5d02b660e66ce
Renato Santos <renato_jfs@hotmail.com>
2025-03-11 23:18:57 -0300

    feat: Pegando os dados e lidando com o submit do form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { Button } from '../Button';
@@ -8,22 +9,51 @@ import { Select } from '../Select';
 import { Form, ButtonContainer } from './styles';
 
 export function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log({
+      name, email, phone, category,
+    });
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
-        <Input placeholder="Nome" />
-      </FormGroup>
-
-      <FormGroup error="O formato do e-mail é inválido.">
-        <Input placeholder="E-mail" error />
-      </FormGroup>
-
-      <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="Nome"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Input
+          placeholder="E-mail"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">Selecione</option>
           <option value="instagram">Instagram</option>
           <option value="linkedin">LinkedIn</option>
         </Select>
