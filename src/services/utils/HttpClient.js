af38a645025b0a6247f2de5fcf27fa134f25e145
Renato Santos <renato_jfs@hotmail.com>
2025-03-22 00:31:47 -0300

    feat: Tratando os erros retornados pela API

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -6,11 +6,15 @@ class HttpClient {
   }
 
   async get(path) {
-    const response = await fetch(`${this.baseURL}${path}`);
-
     await delay(2000);
 
-    return response.json();
+    const response = await fetch(`${this.baseURL}${path}`);
+
+    if (response.ok) {
+      return response.json();
+    }
+
+    throw new Error(`${response.status} - ${response.statusText}`);
   }
 }
 
