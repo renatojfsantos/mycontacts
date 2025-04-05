cba894da1f6d389d1d42d617d6e26302dd80a90c
Renato Santos <renato_jfs@hotmail.com>
2025-04-05 16:01:07 -0300

    feat: Criando event emitter

--- src/components/Toast/ToastContainer/index.js
+++ src/components/Toast/ToastContainer/index.js
@@ -1,13 +1,24 @@
-import { Container } from './styles';
+import { useState } from 'react';
 
 import { ToastMessage } from '../ToastMessage';
 
+import { Container } from './styles';
+
 export function ToastContainer() {
+  const [messages] = useState([
+    { id: Math.random(), type: 'default', text: 'Default toast' },
+    { id: Math.random(), type: 'danger', text: 'Danger toast' },
+    { id: Math.random(), type: 'success', text: 'Success toast' },
+  ]);
   return (
     <Container>
-      <ToastMessage text="Default toast" />
-      <ToastMessage text="Error toast" type="danger" />
-      <ToastMessage text="Success toast" type="success" />
+      {messages.map((message) => (
+        <ToastMessage
+          key={message.id}
+          text={message.text}
+          type={message.type}
+        />
+      ))}
     </Container>
   );
 }
