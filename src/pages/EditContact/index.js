97129725cc0e313b87954abd696d371745d50ab0
Renato Santos <renato_jfs@hotmail.com>
2025-05-24 22:12:03 -0300

    feat: Carregando os dados do contato na página de edição

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -1,15 +1,53 @@
+import { useEffect, useState } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+
+import ContactsService from '../../services/ContactsService';
+import { toast } from '../../utils/toast';
+
 import { ContactForm } from '../../components/ContactForm';
 import { PageHeader } from '../../components/PageHeader';
+import { Loader } from '../../components/Loader';
 
 export function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id);
+
+        console.log({ contactData });
+        setIsLoading(false);
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  function handleSubmit() {
+    //
+  }
+
   return (
     <>
+      <Loader isLoading={isLoading} />
+
       <PageHeader
         title="Editar Renato José"
       />
 
       <ContactForm
         buttonLabel="Salvar alterações"
+        onSubmit={handleSubmit}
       />
     </>
   );
