cb8da6add5db678d704f8ab2b41c5106a1ca411c
Renato Santos <renato_jfs@hotmail.com>
2025-05-26 16:25:59 -0300

    feat: Implementando a edição de contatos

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -37,8 +37,28 @@ export function EditContact() {
     loadContact();
   }, [id, history]);
 
-  function handleSubmit() {
-    //
+  async function handleSubmit(formData) {
+    const contact = {
+      name: formData.name,
+      email: formData.email,
+      phone: formData.phone,
+      category_id: formData.categoryId,
+    };
+
+    try {
+      const contactData = await ContactsService.updateContact(id, contact);
+
+      setContactName(contactData.name);
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso!',
+      });
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao editar o contato!',
+      });
+    }
   }
 
   return (
