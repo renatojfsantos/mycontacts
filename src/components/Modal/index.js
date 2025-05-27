5378198baa251f41bd62418498b9adfcbd5a9a06
Renato Santos <renato_jfs@hotmail.com>
2025-05-27 10:06:58 -0300

    feat: Melhorando o uso dos React Portals

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,8 +1,8 @@
-import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 import { Button } from '../Button';
 import { Overlay, Container, Footer } from './styles';
+import { ReactPortal } from '../ReactPortal';
 
 export function Modal({
   danger,
@@ -19,37 +19,38 @@ export function Modal({
     return null;
   }
 
-  return ReactDOM.createPortal(
-    <Overlay>
-      <Container danger={danger}>
-        <h1>{title}</h1>
+  return (
+    <ReactPortal containerId="modal-root">
+      <Overlay>
+        <Container danger={danger}>
+          <h1>{title}</h1>
 
-        <div className="modal-body">
-          {children}
-        </div>
+          <div className="modal-body">
+            {children}
+          </div>
 
-        <Footer>
-          <button
-            type="button"
-            className="cancel-button"
-            onClick={onCancel}
-            disabled={isLoading}
-          >
-            {cancelLabel}
-          </button>
+          <Footer>
+            <button
+              type="button"
+              className="cancel-button"
+              onClick={onCancel}
+              disabled={isLoading}
+            >
+              {cancelLabel}
+            </button>
 
-          <Button
-            type="button"
-            danger={danger}
-            onClick={onConfirm}
-            isLoading={isLoading}
-          >
-            {confirmLabel}
-          </Button>
-        </Footer>
-      </Container>
-    </Overlay>,
-    document.getElementById('modal-root'),
+            <Button
+              type="button"
+              danger={danger}
+              onClick={onConfirm}
+              isLoading={isLoading}
+            >
+              {confirmLabel}
+            </Button>
+          </Footer>
+        </Container>
+      </Overlay>
+    </ReactPortal>
   );
 }
 
