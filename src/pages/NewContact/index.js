68f82867ad486941675ffe6c6d3f5628ccbeb8c1
Renato Santos <renato_jfs@hotmail.com>
2025-05-29 15:23:46 -0300

    feat: Iniciando a criação do ContactMapper

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+
 import ContactsService from '../../services/ContactsService';
 
 import { ContactForm } from '../../components/ContactForm';
@@ -8,15 +9,8 @@ import { toast } from '../../utils/toast';
 export function NewContact() {
   const contactFormRef = useRef(null);
 
-  async function handleSubmit(formData) {
+  async function handleSubmit(contact) {
     try {
-      const contact = {
-        name: formData.name,
-        email: formData.email,
-        phone: formData.phone,
-        category_id: formData.categoryId,
-      };
-
       await ContactsService.createContact(contact);
 
       contactFormRef.current.resetFields();
